7aaebe19898a7a3dafea8a44267205712e95bd5d
eremsimsek <eremsmsk@gmail.com>
2021-04-08 16:27:37 +0300

    Initial Commit

---
+++ Assets/Instatiate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Instatiate : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerPrefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector3(Random.Range(-4.0f, 4.0f), 2f, 0), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Menu.cs
@@ -0,0 +1,70 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Menu : MonoBehaviourPunCallbacks
+{
+
+    private bool isConnecting = false;
+    private const string GameVersion = "0.1";
+    private const int MaxPlayerPerRoom = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void SearchForGame()
+    {
+        isConnecting = true;
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        else
+        {
+            PhotonNetwork.GameVersion = GameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connectedmaster");
+
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log($"Disconnected due to: {cause}");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No clients have made any rooms");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayerPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Client succesfully joined room");
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
